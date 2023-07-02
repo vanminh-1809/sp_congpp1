caa97e416736a438adc584bb7c991d4d55e42fbb
CongPp1 <103237012+CongPp1@users.noreply.github.com>
2023-07-02 22:20:21 +0700

    Footer

--- src/containers/Body.jsx
+++ src/containers/Body.jsx
@@ -22,12 +22,12 @@ const Body = () => {
 
   return (
     <Fragment>
-      <div className="body-box" style={{display: 'flex', margin: 0, width: "100%"}}>
-        <div style={{margin: 'auto', width: '70%'}}>
-            Contacts
-            <div className="home-link">
-              <a href="/">Home</a>
-            </div>
+      <div className="body-box" style={{ display: 'flex', margin: 0, width: "100%" }}>
+        <div style={{ margin: 'auto', width: '70%' }}>
+          Contacts
+          <div className="home-link">
+            <a href="/">Home</a>
+          </div>
         </div>
       </div>
       <div
@@ -83,6 +83,12 @@ const Body = () => {
           </div>
         </div>
       </div>
+      <div className="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.3674681433736!2d105.78172491121074!3d21.017977780547866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab54f2982fbf%3A0x3a1fa0bffdd98df0!2zVMOyYSBuaMOgIE1pdGVjLCBT4buRIDEgRMawxqFuZyDEkMOsbmggTmdo4buHLCBN4buFIFRyw6wsIEPhuqd1IEdp4bqleSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1688307581111!5m2!1svi!2s" 
+        width="100%" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+      </div>
+
     </Fragment>
   );
 };
--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,9 +1,65 @@
 import { Fragment } from "react"
+import logofooter from "../../src/asset/logofooter.png"
+import Button from "../components/Button"
 
 const Footer = () => {
+    const buttonText = [
+        { text: 'Home', active: false },
+        { text: 'Services', active: false },
+        { text: 'Contacts', active: false },
+    ];
+
     return (
         <Fragment>
-            Footer
+            <div className="footer-container" >
+                <div className="footer-container-comp" >
+                    <div className="footer-comp-1">
+                        <div className="img-footer">
+                            <img src={logofooter} alt="logoFooter" />
+                        </div >
+                        <div className="link-list">
+                            <div className="linkedin-link">
+                                <a href="https://www.linkedin.com/company/vggate">LinkedIn</a>
+                            </div>
+                            <div className="github-link">
+                                <a href="https://github.com/Vggate">Git Hub</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-comp-2">
+                        <h6>Our Address:</h6>
+                        <p>7th Floor, MITEC Tower</p>
+                        <p>Duong Dinh Nghe Street</p>
+                        <p>Cau Giay District</p>
+                        <p>Hanoi, Vietnam</p>
+                    </div>
+                    <div className="footer-comp-3">
+                        <div className="footer-comp-3-mailbox">
+                            <h6>Our Mailbox</h6>
+                            <p>sales@vggate.com</p>
+                        </div>
+                        <div className="footer-comp-3-phone">
+                            <h6>Our Phone</h6>
+                            <p>+84 947645153</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-menu-container" >
+                    <div style={{ display: 'flex', justifyContent: 'center', width: '50%' }}>
+                        {buttonText.map((button) => (
+                            <Button
+                                className={button.active ? 'btn-header-active' : 'btn-header'}
+                                key={button}
+                                text={button.text}
+                                textColor="text-black"
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="sign">
+                    <p>Copyright © 2023 VGGate. All Rights Reserved.</p>
+                </div>
+            </div>
         </Fragment>
     )
 }
